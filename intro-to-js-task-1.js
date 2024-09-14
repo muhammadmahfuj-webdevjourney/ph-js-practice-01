d0eed3e7c30713476e0ba5a51b03922ba1572d9b
muhammad mahfuj <muhammad.mahfuj101@gmail.com>
2024-09-14 20:26:37 +0600

    Codewithmosh exercise done

--- intro-to-js-task-1.js
+++ intro-to-js-task-1.js
@@ -1,16 +1,28 @@
-// This imports prompt sync for user input in the terminal
-const prompt = require('prompt-sync')();
+// // This imports prompt sync for user input in the terminal
+// const prompt = require('prompt-sync')();
 
-// Taking inputs from the user
-const startingMoney = parseFloat(prompt("How much Taka you took to market: "));
-const orangePrice = parseFloat(prompt("Price of 1 kg of oranges and 1 kg of Oranges: "));
-const applePrice = parseFloat(prompt("Price of 1 kg of oranges and 1 kg of apples: "));
+// // Taking inputs from the user
+// const startingMoney = parseFloat(prompt("How much Taka you took to market: "));
+// const orangePrice = parseFloat(prompt("Price of 1 kg of oranges and 1 kg of Oranges: "));
+// const applePrice = parseFloat(prompt("Price of 1 kg of oranges and 1 kg of apples: "));
 
-// Calculate the total cost
-let totalCost = orangePrice + applePrice;
+// // Calculate the total cost
+// let totalCost = orangePrice + applePrice;
 
-// shopkeeper should return thsi much money
-let returnMoney = startingMoney - totalCost;
+// // shopkeeper should return thsi much money
+// let returnMoney = startingMoney - totalCost;
 
-// Print the result
-console.log("Shopkeeper will return: " + returnMoney);
+// // Print the result
+// console.log("Shopkeeper will return: " + returnMoney);
+
+
+// function  maxNum(x, y) {
+//     if (x>y)return x;
+//     return y;
+// }
+
+// Write a function that takes two numbers and returns the maximum of the two
+function maxNum(x, y) {
+    return (x>y) ? x : y;
+}
+console.log(maxNum(8,20));
